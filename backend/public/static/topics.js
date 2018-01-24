a5038114ad637c91a86ee28a22c6d125d4f00b18
Moey Design <5884seiya@gmail.com>
2018-01-24 12:10:50 -0500

    added backend boilerplate

---
+++ backend/public/static/topics.js
@@ -0,0 +1,8 @@
+export default [
+  "What plants and animals did you discover in the Pine Bush?",
+  "What is your favorite moment when taking a walk in the Pine Bush?",
+  "What season do you think is the best for the Pine Bush and why?",
+  "What were you talking with your family or friends in the Pine Bush today?",
+  "Tell me what you've learned about the Pine Bush.",
+  "Tell me anything you want to share about the Pine Bush with others."
+]
