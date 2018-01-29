c1dc30322f454bc35703b2d7338c7d8813307e86
Moey Design <5884seiya@gmail.com>
2018-01-29 10:28:53 -0500

    adding twitter block

--- frontend-dev/src/page-components/BlockContainer.js
+++ frontend-dev/src/page-components/BlockContainer.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import styles from './BlockContainer.css'
 import MonitorBlock from './MonitorBlock.js'
+import TwitterBlock from './TwitterBlock.js'
 
 class BlockContainer extends Component {
   render () {
     return (
       <ul className={styles.container}>
+        <TwitterBlock />
         <MonitorBlock />
         <MonitorBlock />
         <MonitorBlock />
---
+++ frontend-dev/src/page-components/TwitterBlock.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import styles from './TwitterBlock.css'
+
+class TwitterBlock extends Component {
+  render () {
+    return (
+      <div className={styles.wrapper}>
+      </div>
+    )
+  }
+}
+
+export default TwitterBlock
